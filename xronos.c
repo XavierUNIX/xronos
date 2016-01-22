f9ea59d0321ebdfff7b0b00dbb45824f403c5d18
XavierUNIX <mi.unix.server@gmail.com>
2016-01-21 19:48:38 -0500

    Καλοκαίρια και χειμώνες

--- xronos.c
+++ xronos.c
@@ -1,17 +1,17 @@
 /*
 
    UNIX Use: cc xronos.c -o xronos
-   ./xronos
+             ./xronos; ./xronos 1
 
- */
+*/
 
 #include <stdio.h>
 #include <math.h>
 #include <time.h>
 
-#define NYC    (4)
+#define NYC      (4)
 
-main(){
+main(int argc, char *argv[]){
 
   time_t raw_t;
   signed int mod_t;
@@ -21,8 +21,13 @@ main(){
   xronos = gmtime(&raw_t);
   mod_t = (xronos->tm_hour-NYC);
 
-  (mod_t <= 0)? 
-    printf("\n\n \033[22;32mMachine Time: %2i:%02i\033[0m\n\n", (mod_t % 24+24)%24, (*xronos).tm_min):
-    printf("\n\n \033[22;33mMachine Time: %2i:%02i\033[0m\n\n", mod_t, (*xronos).tm_min);
-
+  if (argc == 2){
+    (mod_t <= 0)? 
+      printf("\n\n \033[22;32mWinter NYC Time: %2i:%02i\033[0m\n\n", ((mod_t % 24+24)%24)-1, (*xronos).tm_min):
+      printf("\n\n \033[22;33mWinter NYC Time: %2i:%02i\033[0m\n\n", mod_t-1, (*xronos).tm_min);
+  } else {
+    (mod_t <= 0)?
+      printf("\n\n \033[22;32mSummer NYC Time: %2i:%02i\033[0m\n\n", (mod_t % 24+24)%24, (*xronos).tm_min):
+      printf("\n\n \033[22;33mSummer NYC Time: %2i:%02i\033[0m\n\n", mod_t, (*xronos).tm_min);
+  }
 }
