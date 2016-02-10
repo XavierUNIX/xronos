09fc60851357124175eac8712e225ad6ea504bb1
XavierUNIX <mi.unix.server@gmail.com>
2016-02-10 00:58:57 -0500

    time manipulation using C

--- xronos.c
+++ xronos.c
@@ -1,7 +1,7 @@
 /*
 
    UNIX Use: cc xronos.c -o xronos
-             ./xronos; ./xronos 1
+             ./xronos
 
 */
 
@@ -21,13 +21,8 @@ main(int argc, char *argv[]){
   xronos = gmtime(&raw_t);
   mod_t = (xronos->tm_hour-NYC);
 
-  if (argc == 2){
-    (mod_t <= 0)? 
-      printf("\n\n \033[22;32mWinter NYC Time: %2i:%02i\033[0m\n\n", (((mod_t-1) % 24+24)%24), (*xronos).tm_min):
-      printf("\n\n \033[22;33mWinter NYC Time: %2i:%02i\033[0m\n\n", mod_t-1, (*xronos).tm_min);
-  } else {
-    (mod_t <= 0)?
-      printf("\n\n \033[22;32mSummer NYC Time: %2i:%02i\033[0m\n\n", (mod_t % 24+24)%24, (*xronos).tm_min):
-      printf("\n\n \033[22;33mSummer NYC Time: %2i:%02i\033[0m\n\n", mod_t, (*xronos).tm_min);
-  }
+  (mod_t <= 0)?
+    printf("\n\n \033[22;32mMachine Time: %2i:%02i:%02i\033[0m\n\n", (mod_t % 24+24)%24, (*xronos).tm_min, (*xronos).tm_sec):
+    printf("\n\n \033[22;32mMachine Time: %2i:%02i:%02i\033[0m\n\n", mod_t, (*xronos).tm_min, (*xronos).tm_sec);
+
 }
